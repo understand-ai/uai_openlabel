83cbb2a37be9b85f34f64c8adae072548c04e9d5
Andrew DeLay <andrew.delay@understand.ai>
2024-03-11 08:58:39 +0100

    Revert "remove unneeded type ignore"
    This reverts commit 05ee2dfc27a379ce54fe337f9b4555c2fd5d1399.

--- test_uai_openlabel/test_asam_examples/test_asam_examples.py
+++ test_uai_openlabel/test_asam_examples/test_asam_examples.py
@@ -12,7 +12,7 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from deepdiff import DeepDiff
+from deepdiff import DeepDiff  # type: ignore
 
 from test_uai_openlabel.utils_for_tests import get_json_content
 from uai_openlabel import OpenLabel
